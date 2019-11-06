0ec0a092db26cb4f5cd0eaa15eeeaad32264b85c
Felype <felype.oliveira@etec.sp.gov.br>
2019-11-05 21:08:10 -0300

    aaaaaa

--- Agenda/Agenda.cpp
+++ Agenda/Agenda.cpp
@@ -19,6 +19,9 @@ void cadastrar();
 void ordenar();
 void imprimir();
 void sair();
+void salvar();
+void carregar();
+void pesquisar();
 int sel;
 
 void main()
@@ -30,15 +33,31 @@ void main()
 		sel = menu();
 		switch (sel)
 		{
+
 		case 1: cadastrar();
 			break;
-
 		case 2: imprimir();
 			break;
-
-		case 3: sair();
+		case 3: ordenar();
 			break;
-		case 4: ordenar();
+		case 4: salvar();
+			break;
+		case 5: carregar();
+			break;
+		case 6 : pesquisar();
+			break;
+		//case 7: excluir();
+			//break;
+		case 8 : sair();
+			break;
+
+		default: 
+			system("cls");
+			gotoxy(30, 12);
+			printf("Digite um item incorreto!");
+			gotoxy(20, 13);
+			printf("Digite um valor de acordo com o menu!");
+			_getch();
 			break;
 		}
 	}
@@ -47,14 +66,10 @@ void main()
 int menu()
 {
 	int valor_sel;
-	gotoxy(30, 5);
-	printf("Agenda");
-	gotoxy(30, 10);
-	printf("1.Cadastrar");
-	gotoxy(30, 11);
-	printf("2.Imprimir");
-	gotoxy(30, 12);
-	printf("3.Fim");
+	gotoxy(30, 5); printf("Agenda");
+	gotoxy(30, 10); printf("1.Cadastrar");
+	gotoxy(30, 11); printf("2.Imprimir");
+	gotoxy(30, 12); printf("3.Fim");
 	gotoxy(30, 13);
 	printf("4.Ordenar");
 	gotoxy(30, 14);
@@ -143,6 +158,60 @@ void ordenar() {
 
 }
 
+void salvar()
+{
+	int j;
+	FILE *tp;
+	errno_t err;
+	if ((err = fopen_s(&tp, "cadastro.txt", "w")) != NULL) 
+	{
+		system("cls");
+		gotoxy(30, 18);
+		printf("Não foi possível salvar o arquivo");
+		_getch();
+	}
+	else
+	{
+		for (j = 0; j < 4; j++) 
+		{
+			fwrite(&agenda[j], sizeof(agenda), 1, tp);
+			fclose(tp);
+		}
+		system("cls");
+		gotoxy(35, 18);
+		printf("Sucesso!!!!");
+		_getch();
+		system("cls");
+	}
+}
+
+
+void carregar()
+{
+	int j;
+	FILE *tp;
+	errno_t err;
+	if ((err = fopen_s(&tp, "cadastro.txt", "r")) != NULL)
+	{
+		system("cls");
+		gotoxy(30, 18);
+		printf("Não foi possível ler o arquivo");
+		_getch();
+	}
+	else
+	{
+		for (j = 0; j < 4; j++)
+		{
+			fread(&agenda[j], sizeof(agenda), 1, tp);
+			fclose(tp);
+		}
+		system("cls");
+		gotoxy(35, 18);
+		printf("Sucesso!!!!");
+		_getch();
+		system("cls");
+	}
+}
 void sair()
 {
 	exit(1);
